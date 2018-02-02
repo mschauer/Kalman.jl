55d660d5577683aed7d0e286dc556536bc21053c
mschauer <moritzschauer@web.de>
2018-02-02 21:25:43 +0100

    Plot labels

--- test/testplot.jl
+++ test/testplot.jl
@@ -8,7 +8,8 @@ using Plots
 
 Y, X = sample(20, 1, M)
 Xhat = kalmanrts(Y, M)[2]
-scatter(Y[:], label=L"Y (= X_1 = obs)")
+scatter(Y[:], label=L"Y (= X_1 + \epsilon = obs)")
 plot!(reshape(X,2,20)', color = [:red :blue], label=[L"X_1" L"X_2"] )# label=[L"X_1", L"X_2" ])
-plot!(reshape(Xhat,2,20)', line= :dot, color = [:red :blue], label=[L"X_1 (est)" L"X_2 (est)"] )# label=[L"X_1", L"X_2" ])
+plot!(reshape(Xhat,2,20)', line= :dot, color = [:red :blue], label=[L"X_1 (est.)" L"X_2 (est.)"] )# label=[L"X_1", L"X_2" ])
 
+savefig("test/testplot.png")
